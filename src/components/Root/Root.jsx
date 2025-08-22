96d57238e203273301dd2ca580f8d55234593b6c
Niloy150042 <mehedislam150042@gmail.com>
2025-08-22 20:48:40 +0600

    initiating

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+const Root = () => {
+    return (
+        <div className="">
+
+            Hello from book root
+
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './components/Root/Root';
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+
+  <StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </StrictMode>
+
+)
